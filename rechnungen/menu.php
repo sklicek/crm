71edcd26d2274318ac7f152e6937dd31d610112c
Steve Klicek <steveklicek@gmail.com>
2019-12-29 12:05:04 +0100

    Delete menu.php

--- rechnungen/menu.php
+++
@@ -1,8 +0,0 @@
-<div data-role="navbar">
-<ul>
-<li><a href="../index.php">Hauptseite</a></li>
-<li><a href="../kunden/kunden.php">Kunden</a></li>
-<li><a href="rechnungen.php">Ausgangsrechnungen</a></li>
-<li><a href="../auswertungen/auswertungen.php">Auswertungen</a></li>
-</ul>
-</div>

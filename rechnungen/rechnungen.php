274de327dafd642628ac951a51f7d4d731f8eb2c
Steve Klicek <steveklicek@gmail.com>
2019-12-29 12:05:26 +0100

    Add files via upload

--- rechnungen/rechnungen.php
+++ rechnungen/rechnungen.php
@@ -7,27 +7,20 @@
 <meta name="description" content="Rechnungen">
 <meta name="robots" content="index,follow">
 <title>Ausgangsrechnungen</title>
-<!-- jquery mobile -->
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+<link rel="stylesheet" href="../css/style.css" />
 </head>
 <body>
 <?php
-include("menu.php");
+include("../include/menu.php");
 @require_once("../include/config.inc.php");
 ?>
-<h1>Ausgangsrechnungen</h1>
-<p>
-<div class="ui-block-a">
-  <a href="rechnungen_bearbeiten.php?action=n" data-role="button" data-icon="plus">Neue Rechnung</a><br/>
-</div>
+<div class="table">
+<p class="header">Ausgangsrechnungen
+  <a class="btn" href="rechnungen_bearbeiten.php?action=n">Neue Rechnung</a>
 </p>
-
-<table data-role="table" class="ui-responsive table-stroke">
+<table>
   <thead>
     <tr>
-     <th>ID</th>
      <th>Nr</th>
      <th>Datum Rechn.</th>
      <th>Datum Leistung</th>
@@ -52,15 +45,14 @@ if ($stmt2 = $mysqli -> prepare("SELECT a.id_rechnung, a.rechnung_nr, a.rechnung
     $total+=$brutto;
     ?>
     <tr>
-    <td><a href="rechnungen_bearbeiten.php?id=<?=$id;?>&action=e"><?=$id;?></a></td>
- 	 <td>
-    	<?php
+    <td>
+    <?php
 		if ($pdffile!=""){
 			?>
 			<a href="<?=$pdffile;?>" target="_blank"><?=$nr;?></a>
 			<?php
 		} else {
-			echo $nr;
+            echo $nr;
 		}
 		?>
     </td>
@@ -70,8 +62,9 @@ if ($stmt2 = $mysqli -> prepare("SELECT a.id_rechnung, a.rechnung_nr, a.rechnung
     <td><?=$kunde;?></td>
     <td><?=$dat_bez;?></td>
     <td>
-		<a onclick="return confirm('Definitif löschen ?');" href="rechnungen_bearbeiten.php?id=<?=$id;?>&action=d" data-role="button" data-mini="true">Löschen</a>
-		<a href="upload_file.php?id_rechnung=<?=$id;?>&nr=<?=$nr;?>" data-role="button" data-mini="true">Dateianhang</a>
+		<a class="btn" href="rechnungen_bearbeiten.php?id=<?=$id;?>&action=e">Bearbeiten</a>
+		<a class="btn" href="upload_file.php?id_rechnung=<?=$id;?>&nr=<?=$nr;?>">Dateianhang</a>
+		<a class="btn" onclick="return confirm('Definitif löschen ?');" href="rechnungen_bearbeiten.php?id=<?=$id;?>&action=d">Löschen</a>
 	</td>
     </tr>
     <?php	
@@ -82,10 +75,11 @@ $mysqli->close();
 ?>
 <tr>
  <td colspan="1" style="background-color:lightgrey">Anzahl: <?=$counter;?></td>
- <td colspan="3" style="background-color:lightgrey">&nbsp;</td>
- <td colspan="4" style="background-color:lightgrey"><b><?=number_format($total,2);?></b></td>
+ <td colspan="2" style="background-color:lightgrey">&nbsp;</td>
+ <td colspan="5" style="background-color:lightgrey"><b><?=number_format($total,2);?></b></td>
 </tr>
 </tbody>
 </table>
+</div>
 </body>
 </html>

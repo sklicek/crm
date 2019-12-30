03d7402d7347edb01901d970622c41981b424991
Steve Klicek <steveklicek@gmail.com>
2019-12-30 08:53:57 +0100

    Add files via upload

--- include/menu.php
+++ include/menu.php
@@ -3,17 +3,33 @@ session_start();
 ?>
 <ul class="topnav">
 <li><a href="<?=$_SESSION['root_path'];?>/index.php">Hauptseite</a></li>
-<li><a href="<?=$_SESSION['root_path'];?>/kunden/kunden.php">Kunden</a></li>
-<li><a href="<?=$_SESSION['root_path'];?>/lieferanten/lieferanten.php">Lieferanten</a></li>
-<li><a href="<?=$_SESSION['root_path'];?>/rechnungen/rechnungen.php">Ausgangsrechnungen</a></li>
-<li><a href="<?=$_SESSION['root_path'];?>/rechnungen_eingang/rechnungen.php">Eingangsrechnungen</a></li>
-<li><a href="<?=$_SESSION['root_path'];?>/kontenrahmen/kontenrahmen.php">Kontenrahmen</a></li>
+<li class="dropdown">
+  <a href="javascript:void(0)" class="dropbtn">Kontakte</a>
+  <div class="dropdown-content">
+    <a href="<?=$_SESSION['root_path'];?>/kunden/kunden.php">Kunden</a>
+    <a href="<?=$_SESSION['root_path'];?>/lieferanten/lieferanten.php">Lieferanten</a>
+  </div>
+</li>
+<li class="dropdown">
+  <a href="javascript:void(0)" class="dropbtn">Rechnungen</a>
+  <div class="dropdown-content">
+	<a href="<?=$_SESSION['root_path'];?>/rechnungen/rechnungen.php">Ausgangsrechnungen</a>
+	<a href="<?=$_SESSION['root_path'];?>/rechnungen_eingang/rechnungen.php">Eingangsrechnungen</a>
+  </div>
+</li>
 <li class="dropdown">
   <a href="javascript:void(0)" class="dropbtn">Auswertungen</a>
   <div class="dropdown-content">
       <a href="<?=$_SESSION['root_path'];?>/auswertungen/eur.php">EÜR</a>
       <a href="<?=$_SESSION['root_path'];?>/auswertungen/rechnungen_view.php">Ausgangsrechnungen</a>
       <a href="<?=$_SESSION['root_path'];?>/auswertungen/rechnungen_eingang_view.php">Eingangsrechnungen</a>
+  </div>
+</li>
+<li class="dropdown">
+  <a href="javascript:void(0)" class="dropbtn">Konfiguration</a>
+  <div class="dropdown-content">
+      <a href="<?=$_SESSION['root_path'];?>/config/unternehmen.php">Unternehmen</a>
+	  <a href="<?=$_SESSION['root_path'];?>/kontenrahmen/kontenrahmen.php">Kontenrahmen</a>
   </div>
 </li>
 </ul>

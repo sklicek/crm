5e4bc2474e11f6969011bec40e04ed71a40d9de7
Steve Klicek <steveklicek@gmail.com>
2020-01-05 11:55:18 +0100

    Add files via upload

--- js/funktionen.js
+++ js/funktionen.js
@@ -86,3 +86,13 @@
 
 	  return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
 	}
+
+	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  } 

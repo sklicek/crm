5c25e6dacaa08e4d32b9bdcfc64cf40f16c4eb8e
Steve Klicek <steveklicek@gmail.com>
2020-01-05 11:54:51 +0100

    Add files via upload

--- include/menu.php
+++ include/menu.php
@@ -1,43 +1,44 @@
 <?php
 session_start();
 ?>
-<ul class="topnav">
-<li><a href="<?=$_SESSION['root_path'];?>/index.php">Hauptseite</a></li>
-<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Kontakte</a>
+<div class="topnav" id="myTopnav">
+<a href="<?=$_SESSION['root_path'];?>/index.php">Hauptseite</a>
+<div class="dropdown">
+  <button class="dropbtn">Kontakte</button>
   <div class="dropdown-content">
     <a href="<?=$_SESSION['root_path'];?>/kunden/kunden.php?typ=K">Kunden</a>
     <a href="<?=$_SESSION['root_path'];?>/kunden/kunden.php?typ=L">Lieferanten</a>
   </div>
-</li>
-<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Angebote</a>
+</div>
+<div class="dropdown">
+  <button class="dropbtn">Angebote</button>
   <div class="dropdown-content">
-      <a href="<?=$_SESSION['root_path'];?>/config/einheiten.php">Einheiten konfig.</a>
+    <a href="<?=$_SESSION['root_path'];?>/config/einheiten.php">Einheiten konfig.</a>
 	  <a href="<?=$_SESSION['root_path'];?>/config/artikel.php">Artikel konfig.</a>
 	  <a onclick="alert('Diese Funktion ist noch nicht vollständig entwickelt!');" href="<?=$_SESSION['root_path'];?>/angebote/angebote.php">Angebote</a>
   </div>
-</li>
-<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Rechnungen</a>
+</div>
+<div class="dropdown">
+  <button class="dropbtn">Rechnungen</button>
   <div class="dropdown-content">
-	<a href="<?=$_SESSION['root_path'];?>/rechnungen/rechnungen.php">Ausgangsrechnungen</a>
-	<a href="<?=$_SESSION['root_path'];?>/rechnungen_eingang/rechnungen.php">Eingangsrechnungen</a>
+	  <a href="<?=$_SESSION['root_path'];?>/rechnungen/rechnungen.php">Ausgangsrechnungen</a>
+	  <a href="<?=$_SESSION['root_path'];?>/rechnungen_eingang/rechnungen.php">Eingangsrechnungen</a>
   </div>
-</li>
-<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Auswertungen</a>
+</div>
+<div class="dropdown">
+  <button class="dropbtn">Auswertungen</button>
   <div class="dropdown-content">
       <a href="<?=$_SESSION['root_path'];?>/auswertungen/eur.php">EÜR</a>
       <a href="<?=$_SESSION['root_path'];?>/auswertungen/rechnungen_view.php">Ausgangsrechnungen</a>
       <a href="<?=$_SESSION['root_path'];?>/auswertungen/rechnungen_eingang_view.php">Eingangsrechnungen</a>
   </div>
-</li>
-<li class="dropdown">
-  <a href="javascript:void(0)" class="dropbtn">Konfiguration</a>
+</div>
+<div class="dropdown">
+  <button class="dropbtn">Konfiguration</button>
   <div class="dropdown-content">
       <a href="<?=$_SESSION['root_path'];?>/config/unternehmen.php">Unternehmen</a>
-	  <a href="<?=$_SESSION['root_path'];?>/kontenrahmen/kontenrahmen.php">Kontenrahmen</a>
+	    <a href="<?=$_SESSION['root_path'];?>/kontenrahmen/kontenrahmen.php">Kontenrahmen</a>
   </div>
-</li>
-</ul>
+</div>
+<a style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
